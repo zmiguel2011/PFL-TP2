b43473a721d4ffc589ea6b2220bf40bc3353e2f5
zmiguel2011 <93340308+zmiguel2011@users.noreply.github.com>
2023-12-29 16:24:08 +0000

    divided code into several files for better readability

---
+++ src/assembler.hs
@@ -0,0 +1,101 @@
+module Assembler where
+
+import DataStructures
+
+-- STACK FUNCTIONS --
+
+-- Auxiliary function for push an element to the stack
+push :: Value -> Stack -> Stack
+push x xs = x:xs
+
+-- Auxiliary function to pop the first element
+pop :: Stack -> Stack
+pop (x:xs) = xs
+pop _      = error "pop: empty stack"
+
+-- Auxiliary function to retrieve the top element
+top :: Stack -> Value
+top (x:_) = x
+top _     = error "top: empty stack"
+
+-- Auxiliary function to check whether the stack is empty
+isEmpty :: Stack -> Bool
+isEmpty [] = True
+isEmpty _  = False
+
+-- ASSEMBLER FUNCTIONS --
+
+-- Auxiliary function to calculate and push the result of add, mult and sub operations onto the stack
+calc :: Stack -> String -> Stack
+calc stk arg    -- the pattern guard MyInt x <- top stk ensures the top of stack is in fact an Integer (MyInt)
+  | arg == "+", MyInt x <- top stk = push (MyInt (x + intValue (top popStack))) finalStack
+  | arg == "*", MyInt x <- top stk = push (MyInt (x * intValue (top popStack))) finalStack
+  | arg == "-", MyInt x <- top stk = push (MyInt (x - intValue (top popStack))) finalStack
+  | arg == "==", MyInt x <- top stk = push (MyBool (x == intValue (top popStack))) finalStack
+  | arg == "==", MyBool x <- top stk = push (MyBool (x == boolValue (top popStack))) finalStack
+  | arg == "<=", MyInt x <- top stk = push (MyBool (x <= intValue (top popStack))) finalStack
+  | arg == "&&", MyBool x <- top stk = push (MyBool (x && boolValue (top popStack))) finalStack
+  | arg == "neg", MyBool x <- top stk = push (MyBool (not x)) popStack
+  | otherwise = error "Run-time error"
+  where
+    intValue (MyInt x)   = x
+    intValue _           = error "Invalid operand type"
+    boolValue (MyBool x) = x
+    boolValue _          = error "Invalid operand type"
+    popStack             = pop stk
+    finalStack           = pop popStack
+
+-- Auxiliary function to remove a Variable-Value pair in State
+removeVar :: Variable -> [(Variable, Value)] -> [(Variable, Value)]
+removeVar key = filter ((key /=) . fst)
+
+-- Auxiliary function to update a Variable-Value pair in State
+updateVar :: Variable -> Value -> [(Variable, Value)] -> [(Variable, Value)]
+updateVar key val = ((key, val) :) . removeVar key
+
+-- Auxiliary function for the fetch-x operation
+fetchVar :: Variable -> Stack -> State -> (Stack, State)
+fetchVar var stack state
+  | Just value <- lookup var state = (push value stack, state)
+  | otherwise                      = error "Run-time error"
+
+-- Auxiliary function for the store-x operation
+storeVar :: Variable -> Stack -> State -> (Stack, State)
+storeVar var stack state
+  | isEmpty stack = error "Run-time error"
+  | Just _ <- lookup var state = (newStack, updateVar var val state)
+  | otherwise                  = (newStack, (var, val) : state)
+  where
+    val           = top stack
+    newStack      = pop stack
+
+-- Main function to interpret the code and run the instructions, returning the stack and the output values in the storage
+run :: (Code, Stack, State) -> (Code, Stack, State)
+run ([], stack, state) = ([], stack, state)
+run (inst:code, stack, state) = case inst of
+  Push n -> run (code, push (MyInt n) stack, state)
+  Add -> run (code, calc stack "+", state)
+  Mult -> run (code, calc stack "*", state)
+  Sub -> run (code, calc stack "-", state)
+  Tru -> run (code, push (MyBool True) stack, state)
+  Fals -> run (code, push (MyBool False) stack, state)
+  Equ -> run (code, calc stack "==", state)
+  Le -> run (code, calc stack "<=", state)
+  And -> run (code, calc stack "&&", state)
+  Neg -> run (code, calc stack "neg", state)
+  Fetch var -> fetch var
+  Store var -> store var
+  Noop -> run (code, stack, state)
+  Branch c1 c2 -> branch c1 c2
+  Loop c1 c2 -> run (c1 ++ [Branch (c2 ++ [Loop c1 c2]) [Noop]] ++ code, stack, state)
+  where
+    fetch var = let (newStack, newState) = fetchVar var stack state
+                in run (code, newStack, newState)
+    store var = let (newStack, newState) = storeVar var stack state
+                in run (code, newStack, newState)
+    branch c1 c2
+      | MyBool True <- top stack = run (c1 ++ code, newStack, state)
+      | MyBool False <- top stack = run (c2 ++ code, newStack, state)
+      | otherwise = error "Run-time error"
+      where
+        newStack = pop stack
---
+++ src/dataStructures.hs
@@ -0,0 +1,84 @@
+module DataStructures where
+
+import Data.List (sortBy, intercalate)
+
+----- Assembler Data Structures -----
+
+-- Data type for machine instructions
+data Inst =
+  Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | Store String | Noop |
+  Branch Code Code | Loop Code Code
+  deriving Show
+-- Data type for machine code
+type Code = [Inst]
+
+-- Data type for variables
+type Variable = String              -- Variable name
+-- Data type for values
+data Value 
+  = MyInt Integer                   -- Integer value
+  | MyBool Bool                     -- Boolean value
+  deriving Show
+-- Data type for the machine stack
+type Stack = [Value]
+-- Data type for the machine state (storage)
+type State = [(Variable, Value)]    -- Variable name and value
+
+-- Auxiliary function to create an empty stack
+createEmptyStack :: Stack
+createEmptyStack = []
+
+-- Auxiliary function to extract the actual value from a Value data type
+showVal :: Value -> String
+showVal (MyInt intVal) = show intVal
+showVal (MyBool True)  = "True"
+showVal (MyBool False) = "False"
+
+-- Auxiliary function to convert the stack to a string
+stack2Str :: Stack -> String
+stack2Str []     = ""
+stack2Str [x]    = showVal x
+stack2Str (x:xs) = showVal x ++ "," ++ stack2Str xs  
+
+-- Auxiliary function to create an empty stack
+createEmptyState :: State
+createEmptyState = []
+
+-- Auxiliary function to convert the state to a string
+state2Str :: State -> String
+state2Str state = intercalate "," [var ++ "=" ++ showVal val | (var, val) <- sortedState]
+  where
+    sortedState = sortBy (\(var, _) (val, _) -> compare var val) state
+
+
+----- Compiler Data Structures -----
+
+-- Data type for arithmetic expressions
+data Aexp
+  = Var String                  -- Variable
+  | Num Integer                 -- Numeric constant
+  | AddA Aexp Aexp              -- Addition
+  | SubA Aexp Aexp              -- Subtraction
+  | MultA Aexp Aexp             -- Multiplication
+  deriving Show
+
+-- Data type for boolean expressions
+data Bexp
+  = TrueB                       -- Boolean True
+  | FalseB                      -- Boolean False
+  | AndB Bexp Bexp              -- And operation
+  | IntEqual Aexp Aexp          -- Integer Equality comparison
+  | BoolEqual Bexp Bexp         -- Boolean Equality comparison
+  | LessEqual Aexp Aexp         -- Less than or equal comparison
+  | NegB Bexp                   -- Negation
+  deriving Show
+
+-- Data type for statements
+data Stm
+  = Assign String Aexp          -- Assignment: var := Aexp
+  | If Bexp [Stm] [Stm]         -- If-then-else statement: if Bexp then Stm1 else Stm2
+  | While Bexp [Stm]            -- While loop: while Bexp do Stm
+  | NoopStm                     -- No operation
+  | Aexp Aexp                   -- Arithmetic expression
+  | Bexp Bexp                   -- Boolean expression
+  deriving Show
